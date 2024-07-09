152e160f3df61cbad3b992a35de7013c2eb02f39
vin3y <vinaysankar7@gmail.com>
2024-07-09 18:37:36 +0530

    Added mongodb connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use mongodb::{bson::doc, options::ClientOptions, Client};
+use std::env;
 
 #[get("/")]
 async fn home_function() -> impl Responder {
@@ -7,6 +10,26 @@ async fn home_function() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    // println!("{:?}", env::var("MONGO_DB_URI"));
+
+    let mongo_db_connection: String = env::var("MONGO_DB_URI").expect("No env module found");
+    let mut client_options: ClientOptions =
+        ClientOptions::parse(&mongo_db_connection).await.unwrap();
+
+    let client = Client::with_options(client_options).unwrap();
+    match client
+        .database("admin")
+        .run_command(doc! {"ping": 1}, None)
+        .await
+    {
+        Ok(_) => println!("Connected successfully to MongoDB!"),
+        Err(e) => eprintln!("Failed to connect to MongoDB: {}", e),
+    }
+
+    // println!("Connected successfully to mongodb!");
+
     HttpServer::new(|| App::new().service(home_function))
         .bind("localhost:8080")?
         .run()
