bb16330e00cd1ce80cb94ae02b4332474ea09547
vin3y <vinaysankar7@gmail.com>
2024-07-09 17:42:42 +0530

    Added watch module and mongodb module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
+#[get("/")]
 async fn home_function() -> impl Responder {
-    HttpResponse::Ok().body("Hello, World!")
+    HttpResponse::Ok().json("Hello World")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(home_function)))
+    HttpServer::new(|| App::new().service(home_function))
         .bind("localhost:8080")?
         .run()
         .await
