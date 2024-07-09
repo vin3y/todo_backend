9ab99633cc5d5e57cf61a3fb18e698423726c8e5
vin3y <vinaysankar7@gmail.com>
2024-07-09 19:25:38 +0530

    Created default 404 route

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,26 @@ async fn home_function() -> impl Responder {
     HttpResponse::Ok().json("Hello World")
 }
 
+async fn not_found_route_code() -> impl Responder {
+    HttpResponse::NotFound().json(serde_json::json!(
+        {
+            "status":"failed",
+            "statusError":404,
+            "message" : "The route not found"
+        }
+    ))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let state = web::Data::new(create_db_client().await);
-    HttpServer::new(move || App::new().app_data(state.clone()).service(home_function))
-        .bind("localhost:8080")?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .service(home_function)
+            .default_service(web::route().to(not_found_route_code))
+    })
+    .bind("localhost:8080")?
+    .run()
+    .await
 }
